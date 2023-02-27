cd571550f2f052a56c7ba8fb2191a8eeeb07952d
VelizarKD <vlzrft@gmail.com>
2023-02-27 14:37:29 +0200

    new commit

---
+++ src/main/java/com/wedding/weddingappsite/WeddingAppSiteApplication.java
@@ -0,0 +1,13 @@
+package com.wedding.weddingappsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingAppSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingAppSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wedding/weddingappsite/WeddingAppSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.weddingappsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingAppSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
